dcd381e32f969d66605ef20920bc7c54a5776880
Yatharth-14 <yatharthyadav123@gmail.com>
2024-07-13 12:03:34 +0530

    created data folder

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
-import { BentoGrid } from './BentoGrid'
+import { BentoGrid, BentoGridItem } from './BentoGrid'
 
 const Grid = () => {
   return (
     <section id='about'>
-      <BentoGrid></BentoGrid>
+      <BentoGrid>
+        {[
+            { title: 'Title1', description: 'Description', id:1}
+        ].map((item, i)=>(
+            <BentoGridItem 
+            id={item.id}
+            key={item.id}
+            title={item.title}
+            description={item.description}
+            ></BentoGridItem>
+        )
+        )}
+      </BentoGrid>
     </section>
   )
 }
