bc90a215e50e154473b36014098b7b402c97d94d
Yatharth-14 <yatharthyadav123@gmail.com>
2024-07-14 13:09:24 +0530

    first commit of the day

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -2,7 +2,6 @@ import { cn } from "@/utils/cn";
 import { BackgroundGradientAnimation } from "./GradientBg";
 import { GlobeDemo } from "./GridGlobe";
 
-
 export const BentoGrid = ({
   className,
   children,
@@ -46,7 +45,7 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        "row-span-1 relative overflow-hidden rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4 ",
+        "row-span-1 relative overflow-hidden rounded-3xl border border-white/[0.1] group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none dark:bg-black dark:border-white/[0.2] bg-white justify-between flex flex-col space-y-4 ",
         className
       )}
       style={{
@@ -55,13 +54,13 @@ export const BentoGridItem = ({
           "linear-gradient(90deg, rgba(2, 0, 36, 1) 0%, rgba(59, 59, 68, 1) 26%, rgba(93, 108, 111, 1) 100%",
       }}
     >
-      <div className={`${id === 6} && 'flex justify-center h-full'`}>
+      <div className={`${id === 6 && "flex justify-center"} h-full`}>
         <div className="w-full h-full absolute">
           {img && (
             <img
               src={img}
               alt={img}
-              className={cn(imgClassName, "object-cover, object-center")}
+              className={cn(imgClassName, "object-cover object-center")}
             ></img>
           )}
         </div>
@@ -75,7 +74,7 @@ export const BentoGridItem = ({
             <img
               src={spareImg}
               alt={spareImg}
-              className={"object-cover, object-center w-full h-full"}
+              className="object-cover object-center w-full h-full"
             ></img>
           )}
         </div>
@@ -88,25 +87,47 @@ export const BentoGridItem = ({
         <div
           className={cn(
             titleClassName,
-            "group-hover/ bento: translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
+            "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
           )}
         >
-          <div className="font-sans font-extralight text-[#c1c2d3] text-sm md:text-xs lg:text-base z-10">
+          <div className="font-sans font-extralight md:max-w-32 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10">
             {description}
           </div>
 
-          <div className="font-sans font-bold text-lg lg:text-3xl max-w-96 ">
+          <div className="font-sans z-10 font-bold text-sm lg:text-3xl max-w-96 ">
             {title}
           </div>
-        </div>
 
-          {id===2 && <GlobeDemo></GlobeDemo>}
+          {id === 2 && <GlobeDemo></GlobeDemo>}
 
-          {id===3 && (
+          {id === 3 && (
             <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
-              <div className=""></div>
+              <div className="flex flex-col gap-3 lg:gap-8">
+                {["React.js", "Next.js", "Typecript"].map((item) => (
+                  <span
+                    key={item}
+                    className="lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                  >
+                    {item}
+                  </span>
+                ))}
+                <span className="py-4 px-3 rounded-lg text-center bg-[#10132e]"></span>
+              </div>
+              <div className="flex flex-col gap-3 lg:gap-8">
+              <span className="py-4 px-3 rounded-lg text-center bg-[#10132e]"></span>
+                {["MongoDB", "MySql", "Java"].map((item) => (
+                  <span
+                    key={item}
+                    className="lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                  >
+                    {item}
+                  </span>
+                ))}
+                
+              </div>
             </div>
           )}
+        </div>
       </div>
     </div>
   );
