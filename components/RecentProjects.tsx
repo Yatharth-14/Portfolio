216477b7da1899ff455b0b952d75c39331be5e48
Yatharth-14 <yatharthyadav123@gmail.com>
2024-07-15 16:57:59 +0530

    completed projects section

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,46 +1,64 @@
 import { projects } from "@/data";
 import React from "react";
 import { PinContainer } from "./ui/PinContainer";
+import { FaLocationArrow } from "react-icons/fa";
 
 const RecentProjects = () => {
   return (
-    <div className="py-20">
+    <div className="py-20" id="projects">
+      {/* heading of project section */}
       <h1 className="heading">
         A small selection of {""}
         <span className="text-purple">recent projects</span>
       </h1>
-      <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
-        {projects.map(({id, title, des, img, iconLists, link}) => (
-          <div key={id} className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"><PinContainer title={title} href={link}>
-            <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
+      {/* div contaning projects */}
+      <div className="flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10">
+        {projects.map(({ id, title, des, img, iconLists, link }) => (
+          <div
+            key={id}
+            className=" sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem] flex items-center justify-center sm:w-[370px] w-[100vw]"
+          >
+            <PinContainer title={link} href={link}>
+              <div className=" relative flex items-center justify-center sm:w-[370px] w-[80vw] overflow-hidden sm:h-[40vh] h-[30vh] mb-10">
                 <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
-                    <img src="/bg.png" alt="bg-img" />
+                  <img src="/bg.png" alt="bg-img" />
                 </div>
                 <img src={img} alt={title} className="z-10 absoulte bottom-0" />
-            </div>
-            <h1 className="font-bold lg:text-sm md:text-xl lg:text-2xl text base line-clamp-1">
+              </div>
+              <h1 className="font-bold lg:text-sm md:text-xl lg:text-2xl text base line-clamp-1">
                 {title}
-            </h1>
+              </h1>
 
-            <p className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2">
+              <p className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2">
                 {des}
-            </p>
+              </p>
 
-            <div className="flex items-center justify-between mt-7 mb-3">
+              <div className="flex items-center justify-between mt-7 mb-3">
                 <div className="flex items-center">
-                    {iconLists.map((icon) => (
-                        <div
-                        key={icon}
-                        className="border border-white/[0.2]"
-                        >
-                            <img src={icon} alt="" />
-                        </div>
-                    ))}
+                  {iconLists.map((icon, index) => (
+                    <div
+                      key={icon}
+                      className="border border-white/[0.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
+                      style={{
+                        transform: `translateX(-${5 * index * 2}px)`,
+                      }}
+                    >
+                      <img src={icon} alt={icon} className="p-2" />
+                    </div>
+                  ))}
                 </div>
-            </div>
-
+                <div className="flex items-center justify-center">
+                  <p className="flex lg:text-xl md:text-xs text-sm text-purple">
+                    Check Live Site
+                  </p>
+                  <FaLocationArrow
+                    className="ms-3"
+                    color="#cbabf9"
+                  ></FaLocationArrow>
+                </div>
+              </div>
             </PinContainer>
-            </div>
+          </div>
         ))}
       </div>
     </div>
