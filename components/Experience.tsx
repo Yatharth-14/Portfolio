978cec224d36ff6acc7768f625467dc1ae7f1bff
Yatharth-14 <yatharthyadav123@gmail.com>
2024-07-18 14:48:14 +0530

    started work experience section

---
+++ components/Experience.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Experience = () => {
+  return (
+    <div className="py-20" id="testimonials">
+      {/* heading of project section */}
+      <h1 className="heading">
+        Kind words from <span className="text-purple">satisfied clients</span>
+      </h1>
+      {/* div contaning projects */}
+      <div className="flex flex-col items-center max-lg:mt-10"></div>
+    </div>
+  );
+};
+
+export default Experience;
